5efd526796c5e3283565aa395a3c40dd30fd578d
grayhex <grayhex@users.noreply.github.com>
2025-08-21 13:27:46 +0300

    Merge branch 'main' into work

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,14 +1,27 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
   }
 
-  async enableShutdownHooks(app: INestApplication) {
-    (this as any).$on('beforeExit', async () => {
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  enableShutdownHooks(app: INestApplication) {
+    // close Nest on process exit
+    process.on('beforeExit', async () => {
+      await app.close();
+    });
+
+    // handle termination signals from orchestrators
+    process.on('SIGINT', async () => {
+      await app.close();
+    });
+    process.on('SIGTERM', async () => {
       await app.close();
     });
   }
